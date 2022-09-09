cb733cb2665c0f634e5027959664c05f08e284ae
elsab <elsabiru8@gmail.com>
2022-09-09 16:15:02 +0300

    using puts

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	return (0);
+}
