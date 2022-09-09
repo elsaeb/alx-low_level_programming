d6f92c54f3c5711e93c9be0da1d5401323deb30a
elsab <elsabiru8@gmail.com>
2022-09-09 19:25:26 +0300

    update

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,9 +8,13 @@
 
 int main(void)
 {
-	char i;
-	for (i = 'z'; i >= 'a'; i--)
+	int i;
+	char j;
+
+	for (i = '0'; i <= '9'; i++)
 		putchar(i);
+	for (j = 'a'; j <= 'f'; j++)
+		putchar(j);
 	putchar('\n');
 	return (0);
 }
