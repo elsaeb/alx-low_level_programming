d51234d56beec3d9183bfab4fa6adb94b6a604f1
elsab <elsabiru8@gmail.com>
2022-09-20 19:22:07 +0300

    update

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - function that prints everyother string
+ * @str: an input string
+ * Return: Nothing
+ */
+void puts2(char *str)
+{
+	int len = 0, i = 0;
+
+	while (str[len] != '\0')
+		len++;
+
+	len -= 1;
+
+	for (; i <= len; i += 2)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
+
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,5 @@
 void rev_string(char *s);
+void puts2(char *str);
 int _strlen(char *s);
 void swap_int(int *a, int *b);
 void reset_to_98(int *n);
