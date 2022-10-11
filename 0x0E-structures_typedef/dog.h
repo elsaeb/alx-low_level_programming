bf1a62c835c9c84969e7686537bc18ab66daafd9
elsab <elsabiru8@gmail.com>
2022-10-11 16:54:39 +0300

    first one

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure contains character pointer and a float
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
