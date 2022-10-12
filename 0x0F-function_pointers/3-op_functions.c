1b9822f0ed4ed525f5e627263a8cb8fb0eb57bea
elsab <elsabiru8@gmail.com>
2022-10-13 02:25:20 +0300

    all

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,25 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * op_add - a function that returns the sum of a and b.
+ * @a: an input integer
+ * @b: an input integer
+ * Return: The result to sum a + b.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - a function that returns the subtract of a and b.
+ * @a: an input integer
+ * @b: an input integer
+ * Return: The result to substract a - b.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
